146f178641788665fe922e1049c2263bee2a3e63
dimon <dimon@jmail.com>
2020-09-19 18:45:47 +0600

    message

---
+++ 123456/123456/123456.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main()
+    {
+        using namespace std;
+
+        /*
+
+        int UserInt, zero, numbers, sum_2, sum_8;
+        UserInt = zero = numbers = sum_2 = sum_8 = 0;
+        cout << "Write your number" << endl;
+        cin >> UserInt;
+
+        while (zero <= UserInt) {
+            if (zero % 2 == 0) {
+                cout << zero << endl;
+                numbers++;
+                sum_2 = sum_2 + zero;
+            }
+
+            zero++;
+        }
+        zero = 0;
+        while (zero <= UserInt) {
+            if (zero % 8 == 0) {
+                cout << zero << endl;
+                numbers++;
+                sum_8 = sum_8 + zero;
+            }
+
+            zero++;
+        }
+
+
+        cout << "chisel polychilos kratnih 2: " << numbers << endl;
+        cout << "Summa kotoraia deletsa na 2: " << sum_2 << endl;
+
+        cout << "Summa kotoraia deletsa na 8: " << sum_8 << endl;
+
+
+
+        
+
+        srand(time(0));
+        int x = (rand() % 21) + 1;
+        cout << x << endl;
+
+        int UserInt;
+        UserInt = 0;
+        int a = 0;
+        while (a == 0) {
+            cout << "vvedite chislo ot 0 do 100" << endl;
+            cin >> UserInt;
+
+            if (x == UserInt) {
+                cout << "vi ygadali" << endl;
+                break;
+            }
+            if (x > UserInt) {
+                cout << "vashe chislo doljno bit bolshe" << endl;
+            }
+            if (x < UserInt) {
+                cout << "vashe chislo doljno bit menshe" << endl;
+            }
+        }
+        */
+
+        int a = 1;
+        while (a == 1) {
+            int ballance = 1000;
+            int stavka = 1;
+
+            while (stavka) {
+                cin >> stavka;
+                if (stavka <= 0) {
+                cout << "invalid, stavka ne mojet bit menche ili 0!!!" << endl;
+            }
+            if (stavka >=) {
+
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+            system("pause");
+            return 0;
+        }
+    
